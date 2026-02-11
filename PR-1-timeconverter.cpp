0ea3c788196af00dec1e3ee76e09254592098141
harshalvora519-code <harshalvora519@gmail.com>
2026-02-11 14:24:43 +0530

    Add files via upload

---
+++ PR-1-timeconverter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Timeconverter{
+  public:
+    void totime(int totalseconds){
+        int h=totalseconds / 3600;
+        int m=(totalseconds%3600)/60;
+        int s=totalseconds%60;
+        cout<<"HH:MM:SS: => "<<h<<":"<<m<<":"<<s<<endl;
+    }
+    
+     void toseconds(int h, int m, int s){
+     
+         int total = (h * 3600) + (m * 60) + s;
+         cout<<"total seconds:"<<total<<endl;
+     } 
+    };
+    
+    int main(){
+        Timeconverter obj;
+        int choice;
+        cout<<"1.seconds to time\n2.time to seconds\nchoose:";
+        cin>>choice;
+        
+        if(choice == 1){
+            int sec;
+            cout<<"enter total seconds:";
+            cin>>sec;
+            obj.totime(sec);
+        }
+        else if (choice == 2){
+            int h,m,s;
+            cout<<"enter hours:";cin>>h;
+            cout<<"enter minutes:";cin>>m;
+            cout<<"enter seconds:";cin>>s;
+            obj.toseconds(h,m,s);
+        }
+    }
